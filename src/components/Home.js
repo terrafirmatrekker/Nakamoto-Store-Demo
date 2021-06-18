a214d58c283081f88cd21c7033c94e157e26f4bc
Tejas Shastry <tejashastry@gmail.com>
2021-06-17 23:03:25 -0400

    a typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h3 style={{ textAlign: "center" }}>Welcome to the Market!</h3>
+        <h3 style={{ textAlign: "center" }}>Welcome to the Market! To bridge funds via RenJS click Bridge. 
+        <br/>Fill up and head over to Market to buy some goods!</h3>
       </div>
     );
   }
