c6f42bc6fc99b57ee83caa7f37b690a714a42784
Tejas Shastry <11544711+terrafirmatrekker@users.noreply.github.com>
2021-09-03 14:24:27 -0500

    Add files via upload

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,14 +1,45 @@
 pragma solidity ^0.5.0;
 
-// Reading the Product
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+interface IGateway {
+    function mint(
+        bytes32 _pHash,
+        uint256 _amount,
+        bytes32 _nHash,
+        bytes calldata _sig
+    ) external returns (uint256);
+
+    function burn(bytes calldata _to, uint256 _amount)
+        external
+        returns (uint256);
+}
+
+interface IGatewayRegistry {
+    function getGatewayBySymbol(string calldata _tokenSymbol)
+        external
+        view
+        returns (IGateway);
+
+    function getTokenBySymbol(string calldata _tokenSymbol)
+        external
+        view
+        returns (IERC20);
+}
+
 contract Marketplace {
-    string public name; // State variable permanently stored in contract storage https://bit.ly/3pgZn6h
+    string public name; 
     uint256 public productCount = 0;
-    mapping(uint256 => Product) public products; // think of mapping as key-to-value storage, e.g. Dicts, Maps
-    // We can't interate over mapping or use .length, etc, etc..
+    mapping(uint256 => Product) public products;
+    IGatewayRegistry public registry;
+
+    
     struct Product {
-        // We can treat a struct like a value type such that they can be used within arrays and mappings.
-        uint256 id; // List the struct members, which consists of variables names along with their types
+        uint256 id; 
         string name;
         uint256 price;
         address payable owner;
@@ -31,12 +62,31 @@ contract Marketplace {
         bool purchased
     );
 
-    constructor() public {
+    constructor(IGatewayRegistry _registry) public {
         name = "Nakamoto List";
+        registry = _registry;
     }
-
-    // Function to create a product
-    // _ underscores indicate local variables
+    
+    function mint(
+        // Payload
+        string calldata _symbol,
+        address _recipient,
+        // Required
+        uint256 _amount,
+        bytes32 _nHash,
+        bytes calldata _sig
+    ) external {
+        bytes32 payloadHash = keccak256(abi.encode(_symbol, _recipient));
+        uint256 amount = registry.getGatewayBySymbol(_symbol).mint(
+            payloadHash,
+            _amount,
+            _nHash,
+            _sig
+        );
+        registry.getTokenBySymbol("BTC").transfer(msg.sender, amount);
+        
+    }
+    
     function createProduct(string memory _name, uint256 _price) public {
         // Require a valid name
         require(bytes(_name).length > 0);
@@ -57,18 +107,17 @@ contract Marketplace {
     }
 
     function purchaseProduct(uint256 _id) public payable {
-        /*Fetch product (memory keyword: https://bit.ly/3g6fHTn)
-        memory cannot be used at the contract level, only in methods.*/
+        //Get product
         Product memory _product = products[_id];
-        //Fetch the owner
+        //Get the owner
         address payable _seller = _product.owner;
-        //Make sure product is valid (can be bought)
+        //Ensure sure product is valid (can be bought)
         require(_product.id > 0 && _product.id <= productCount);
-        // Require that there is enough Ether in the transaction
+        //Ensure that there is enough Ether in the transaction
         require(msg.value >= _product.price);
-        // Require that the product has not been purchased already
+        //Ensure that the product has not been purchased already
         require(!_product.purchased);
-        // Require that the buyer is not the seller
+        //Ensure that the buyer is not the seller
         require(_seller != msg.sender);
         //Purchase product / transfer ownership
         _product.owner = msg.sender;
@@ -76,7 +125,7 @@ contract Marketplace {
         _product.purchased = true;
         //Update contract
         products[_id] = _product;
-        //Pay seller with Ether
+        //Pay with Ether
         address(_seller).transfer(msg.value);
         //Trigger an event
         emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
